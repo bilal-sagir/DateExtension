a5d7a1e63097d1fda242f7bb10ab106cb75f3b9b
bilal-sagir <sagir.bilal@icloud.com>
2021-12-30 17:20:58 +0300

    add day

--- Contents.swift
+++ Contents.swift
@@ -1,14 +1,37 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let sampleDate: Date = Date(timeIntervalSinceReferenceDate: -12345678.0)
+print(sampleDate)
 
-extension Date{
-    func printB(){print("ll")}
+/*
+extension Date
+{
+    func get(_ components: Calendar.Component...,
+             calendar: Calendar = Calendar.current) -> DateComponents
+    {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    
+    func get(_ component: Calendar.Component,
+             calendar: Calendar = Calendar.current) -> Int
+    {
+        return calendar.component(component, from: self)
+    }
 }
 
-let currentDate = Date()
+print(sampleDate.get(.day))
+print(sampleDate.get(.month))
+print(sampleDate.get(.year))
+*/
 
-print(currentDate)
+extension Date{
+    func nextDate() -> Date{
+        let nextDate = Calendar.current.date(byAdding: .day, value: 1, to: self)
+        return nextDate ?? Date()
+    }
+    
+}
 
-let sampleDate: Date = Date(timeIntervalSinceReferenceDate: -123456789.0)
-print(sampleDate.printB())
+print(sampleDate.nextDate())
