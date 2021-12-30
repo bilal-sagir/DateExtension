5755e67d686cb9e1b87df92f913b006e686c8643
bilal-sagir <sagir.bilal@icloud.com>
2021-12-30 18:15:07 +0300

    done

--- Contents.swift
+++ Contents.swift
@@ -1,37 +1,39 @@
-import UIKit
 import Foundation
 
 let sampleDate: Date = Date(timeIntervalSinceReferenceDate: -12345678.0)
 print(sampleDate)
 
-/*
 extension Date
 {
-    func get(_ components: Calendar.Component...,
-             calendar: Calendar = Calendar.current) -> DateComponents
-    {
-        return calendar.dateComponents(Set(components), from: self)
-    }
-
     
-    func get(_ component: Calendar.Component,
-             calendar: Calendar = Calendar.current) -> Int
-    {
-        return calendar.component(component, from: self)
+    var day: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd"
+        return formatter.string(from: self)
+    }
+    
+    var month: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM"
+        return formatter.string(from: self)
+    }
+    
+    var year: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        return formatter.string(from: self)
+    }
+    
+    var dateAsPrettyString: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd<>MM<>yyyy"
+        return formatter.string(from: self)
     }
 }
 
-print(sampleDate.get(.day))
-print(sampleDate.get(.month))
-print(sampleDate.get(.year))
-*/
 
-extension Date{
-    func nextDate() -> Date{
-        let nextDate = Calendar.current.date(byAdding: .day, value: 1, to: self)
-        return nextDate ?? Date()
-    }
-    
-}
 
-print(sampleDate.nextDate())
+print(sampleDate.day)
+print(sampleDate.month)
+print(sampleDate.year)
+print(sampleDate.dateAsPrettyString)
